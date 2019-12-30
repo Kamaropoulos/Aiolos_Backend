8bd51b6abbf2bf53ee726e3dcaa71f1db7a4767d
Kamaropoulos <kamaropoulos@outlook.com>
2019-12-30 17:18:11 +0200

    fix: LogsController was imported twice

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LogsController } from './logs/logs.controller';
-import { LogsController } from './logs/logs.controller';
 
 @Module({
   imports: [],
