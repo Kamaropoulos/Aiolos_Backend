620b5af16b387441c08e625951513a03987a595d
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2020-01-04 20:49:00 +0200

    feat: Connect to MongoDB
    Co-authored-by: Iro Athina Valagouti <iro_valagouti@yahoo.gr>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LogsController } from './logs/logs.controller';
+import { LogsService } from './logs/logs.service';
+import { LogsModule } from './logs/logs.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(
+    "mongodb://localhost/IoT"
+  ), LogsModule],
   controllers: [AppController, LogsController],
-  providers: [AppService],
+  providers: [AppService, LogsService],
 })
 export class AppModule {}
